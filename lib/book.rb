63e21a9bc124c7b11bbd3c5fff484f75b4c77237
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-08-22 01:13:17 +0300

    commit tree 16ad266f5c8af400c85134b2c0cf5c0f8336d877

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
 
+    attr_reader :title
+    attr_accessor :author, :genre, :page_count
+    
+    def initialize (title = "And Then There Were None")
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
